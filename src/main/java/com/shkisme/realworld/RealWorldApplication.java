c7bcc8becd4b617aa237d477f05570bdaf02bc2a
shkisme <shkisme0130@gmail.com>
2022-09-25 21:55:30 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/shkisme/realworld/RealWorldApplication.java
@@ -0,0 +1,13 @@
+package com.shkisme.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWorldApplication.class, args);
+	}
+
+}
